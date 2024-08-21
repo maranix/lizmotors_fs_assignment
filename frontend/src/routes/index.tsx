d15f26a8712c31e26031fd8e106c1faa64aff5e2
maranix <ramanverma4183@gmail.com>
2024-08-21 13:43:43 +0530

    frontend(components): add TrainingCard component.

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 import { createFileRoute } from "@tanstack/react-router";
+import TrainingCard from "../components/card/TrainingCard";
 
 const Index: React.FC = () => {
     return (
-        <h1 className="flex justify-center text-4xl font-black">
-            React + TanStack
-        </h1>
+        <main className="p-8 mx-auto w-2/3">
+            <h1 className="text-4xl font-black">Available Tracks</h1>
+            <hr />
+
+            <section id="training-tracks">
+                <TrainingCard className="w-1/2" />
+            </section>
+        </main>
     );
 };
 
